d1baa4edb46b089df5364e0b0519a725e3b194f2
Sydrack Gregório <drasyck1@gmail.com>
2019-05-14 16:12:20 -0300

    banco de dados h2 adicionado

--- src/main/java/edu/guru/sydrack/cursomc/domain/Categoria.java
+++ src/main/java/edu/guru/sydrack/cursomc/domain/Categoria.java
@@ -1,11 +1,18 @@
 package edu.guru.sydrack.cursomc.domain;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable{
     
     private static final long serialVersionUID = 1L;
     // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     // Construtores

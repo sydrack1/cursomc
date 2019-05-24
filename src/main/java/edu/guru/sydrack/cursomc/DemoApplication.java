a7df56724bc2c6d846ec64757f1fe1ec30fbfa6a
Sydrack Gregório <drasyck1@gmail.com>
2019-05-24 16:14:40 -0300

    Operação de instanciação

--- src/main/java/edu/guru/sydrack/cursomc/DemoApplication.java
+++ src/main/java/edu/guru/sydrack/cursomc/DemoApplication.java
@@ -1,13 +1,29 @@
 package edu.guru.sydrack.cursomc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import edu.guru.sydrack.cursomc.domain.Categoria;
+import edu.guru.sydrack.cursomc.repositories.CategoriaRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+ 
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository catR;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		catR.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
